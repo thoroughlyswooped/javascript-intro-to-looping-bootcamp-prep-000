1f816db0ad2a70ad42bbdf7b7fd7b5895d174026
Ezra T Huffman <joshooha@gmail.com>
2018-09-20 11:14:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,11 @@
 function forLoop(array) {
   var myString = "";
   for (i = 0; i < 25; i++) {
+    if (i == 1){
+      myString = `I am ${i} stange loop.`;
+    } else {
     myString = `I am ${i} strange loops.`;
+    }
     array.push(myString);
   }
   return array;
