c8b570c207cff5657fbfd9787baa77703e4e7e4b
Ezra T Huffman <joshooha@gmail.com>
2018-09-20 11:22:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,3 +17,10 @@ function whileLoop(n) {
   }
   return "done";
 }
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while (incrementVariable() && array.length > 0);
+  return array;
+}
